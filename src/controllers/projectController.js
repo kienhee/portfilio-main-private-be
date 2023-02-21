e196ae1afdb42ab9c50ae94f046985818c12e746
Tran Trung Kien <kienhee.it@gmail.com>
2023-02-21 19:42:50 +0700

    Update api get all product

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -2,25 +2,37 @@
 
 const Controller = require("./Controller");
 const controller = new Controller("projects");
-const project = require("../models/project")
+const project = require("../models/project");
 
 const handleCreateProject = async (req, res) => {
   try {
     let { title, content, github, urlImage } = req.body;
     if (title && content && github && urlImage) {
-      let response = await project.create({ title, content, github, urlImage })
+      let response = await project.create({ title, content, github, urlImage });
       if (response) {
         return controller.response(res, "Tạo Dự án thành công", 200, response, true);
       } else {
         return controller.response(res, "Tạo Dự án không thành công", 200, response, false);
       }
-
     } else {
       return controller.response(res, "Vui lòng nhập đầy đủ thông tin", 200, [], false);
     }
   } catch (error) {
     return controller.response(res, error.message, error.status, response, false);
   }
-}
+};
 
-module.exports = { handleCreateProject };
+const getAllprojects = async (req, res) => {
+  try {
+    let response = await project.find();
+    if (response) {
+      return controller.response(res, "Lấy dữ liệu thành công", 200, response, true);
+    } else {
+      return controller.response(res, "Lấy dữ liệu thất bại", 200, response, false);
+    }
+  } catch (error) {
+    return controller.response(res, error.message, error.status, response, false);
+  }
+};
+
+module.exports = { handleCreateProject, getAllprojects };
--- src/routes/project.js
+++ src/routes/project.js
@@ -4,8 +4,6 @@ const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
 
-router.post(
-  "/project/create-project",
-  projectController.handleCreateProject
-);
+router.post("/project/create-project", projectController.handleCreateProject);
+router.get("/project/get-all-project", projectController.getAllprojects);
 module.exports = router;
